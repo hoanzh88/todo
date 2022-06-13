7a4c659e4b7209d67b6f6ff9ab482be911212b7b
hoanchuong <hoanchuong@ilavietnam.edu.vn>
2022-06-13 16:52:48 +0700

    sss 17

--- test/pagesTest.php
+++ test/pagesTest.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . "/../controllers/Pages.php";
-class PagesTest extends PHPUnit_Framework_TestCase{
+// class PagesTest extends PHPUnit_Framework_TestCase{
+class PagesTest extends \PHPUnit\Framework\TestCase{
     public function testRenderReturnsHelloWorld(){
         $pages = new \Pages();
         $pages->render();
